8d7a252d2b64e7b6b37cdde233a116504611561f
murilomotta <murilo.gomes.motta@gmail.com>
2017-06-08 16:33:18 +0000

    LAYOUT PAGINA /ADMIN

--- src/Handler/Usuario.hs
+++ src/Handler/Usuario.hs
@@ -178,9 +178,113 @@ postLoginR = do
 getAdminR :: Handler Html
 getAdminR = defaultLayout $ do
     [whamlet|
-        <h1>
-            Bem-vindo Rei dos Reis!!!!!!
+            <linha_topo>   
+            <div id="logo"><img src=@{StaticR header_jpg}>
+            <div id="menu_bg"><div id="menu"> <ul>
+                <li> 
+                    <a href=@{ProfessorCadastrarR}> PROFESSOR
+                <li>
+                    <a href=@{Pag2R}> Página 2
+                <li>
+                    <a href=@{Pag3R}> Página 3
+            <br>
+
+            
+            <div id="content"><div id="sidebar-left"><h1>BEM VINDO!
+            <div id="sidebar-center">
+            <div id="sidebar-right">
+            <div id="footer"><br>© Copyright 2017, Inc. Todos os direitos reservados.<br>
     |]
+    
+    toWidgetHead [lucius|
+            ul {
+                list-style: none;
+                font-family: Arial, Tahoma;
+                height: 40px;
+                width: 900px;
+                margin-left: auto;
+                margin-right: auto;    
+            }
+            li {
+                float:left;
+                padding:12px;
+                font-size:18px;
+            }
+            linha_topo {
+                background-color:#c21d16;
+                position: fixed;
+                top: 0;
+                left: 0;
+                right: 0;
+                height: 10px;
+            }
+            #header {
+                height: 2500px;
+                width: auto;
+                background-color:#ffffff;
+            }
+            #logo {
+                height: 130px;
+                width: 932px;
+                background-color:#ffffff;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            #menu_bg {
+                height: 40px;
+                width: auto;
+                background: url(/static/menu_bg.jpg) repeat-x ;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            #menu {
+                height: 40px;
+                width: 932px;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            #content {
+                height: 400px;
+                width: 932px;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            #sidebar-left {
+                height: 400px;
+                width: 630px;
+                background-color:#ffffff;
+                float:left;
+            }
+            #sidebar-center {
+                height: 400px;
+                width: 2px;
+                background-color:#ffffff;
+                float:left;
+            }
+           #sidebar-right {
+                height: 400px;
+                width: 300px;
+                background-color:#ffffff;
+                float:left;
+            }
+            #footer {
+            	background-color:#c21d16;
+                position: fixed;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                height: 50px;
+                text-align:center;
+                font-size:12px;
+            }
+
+            
+
+
+            body { margin:0; padding:0; font-size:11px; line-height:16px; font-family: Arial, Tahoma;}
+            a, a:visited { color:#283337; text-decoration:none;}
+            a:hover {color:#c21d16; text-decoration:underline;}
+        	    |]
 
 postLogoutR :: Handler Html
 postLogoutR = do
